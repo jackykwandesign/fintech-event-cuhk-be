6aae28f7c087495ffa3359af7d725e49de3045b8
JKxAL <jackykwandesign@gmail.com>
2020-10-10 18:11:19 +0000

    must login to get webinar info

--- src/webinar/webinar.controller.ts
+++ src/webinar/webinar.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/roles.guard';
 import { WebinarService } from './webinar.service';
 
 @Controller('webinar')
+@UseGuards(AuthGuard('bearer'),RolesGuard)
 export class WebinarController {
     constructor(
         private webinarService:WebinarService
